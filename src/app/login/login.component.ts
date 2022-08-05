30bf80ea8e3382783604fab6fc92e6c2aee95f8b
andresbreg <andresbregant@gmail.com>
2022-08-04 22:43:09 -0300

    Login Component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalSwitchService } from './../services/modal-switch.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  modalSwitch!:Boolean;
+  
+  constructor(private modalSwitchService:ModalSwitchService) { }
 
   ngOnInit(): void {
+    this.modalSwitchService.$modal.subscribe((value: Boolean) => {
+      this.modalSwitch = value;
+    });
   }
 
+  closeModal() {
+    this.modalSwitchService.$modal.emit(false);
+  }
+
+  login(form:NgForm) {
+    const email = form.value.email;
+    const password = form.value.password;
+  }
 }
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalSwitchService } from './../services/modal-switch.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
+
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  modalSwitch!:Boolean;
 
-  ngOnInit(): void {
+  constructor(private modalSwitchService:ModalSwitchService) { }
+
+  ngOnInit() {
+    this.modalSwitchService.$modal.subscribe((value: Boolean) => {
+      this.modalSwitch = value;
+    });
   }
 
+  openModal() {
+    this.modalSwitch = true;
+  }
 }
